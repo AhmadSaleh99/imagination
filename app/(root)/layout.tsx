71553fba5498662d7cd36eb3bdf5ba7447ff6949
AhmadSaleh99 <saleehsale816@gmail.com>
2024-07-29 08:33:19 -0500

    clerk and the side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,22 @@
-import React, { ReactNode } from "react";
+import Sidebar from "@/components/shared/Sidebar";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar />
+
       <div className="root-container">
-        <div className="wrapper">{children}</div>
+        <div className="wrapper">
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </div>
       </div>
     </main>
   );
